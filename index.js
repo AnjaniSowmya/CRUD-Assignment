417c5220e5838821284e13df45c3368922e3de5e
AnjaniSowmya <68138735+AnjaniSowmya@users.noreply.github.com>
2021-05-08 22:48:44 +0530

    Added UI

--- index.js
+++ index.js
@@ -1,67 +1,213 @@
-const mysql = require('mysql');
-const express = require('express');
-var app = express();
-const bodyparser = require('body-parser');
-app.use(bodyparser.json());
-
-var mysqlConnection = mysql.createConnection({
-    host:'localhost',
-    user:'AnjaniSowmya',
-    password: 'wtg123@wtg123',
-    database: 'studentdb'
-});
-
-mysqlConnection.connect((err)=>{
-    if(!err)
-    console.log('DB connected Successfully');
-    else
-    console.log('DB connection Failed \n Error : '+JSON.stringify(err,undefined,2));
-    /*var sql = "INSERT INTO students (FirstName, LastName) VALUES ('Anjani Sowmya', 'Bollapragada')";
-    mysqlConnection.query(sql, function (err, result) {
-     if (err) throw err;
-     console.log("1 record inserted");
-    });*/
-});
-
-app.listen(3000, ()=>console.log("Express server is running at port: 3000"));
-app.get('/students', (req,res)=>{
-    var sql = "SELECT * FROM students";
-    mysqlConnection.query(sql, (err, rows, fields)=> {
-     if (err) res.send("No records found!");
-     res.send(rows);
+function onRegister() {
+    var gender = +$("input[name='gender']:checked").val();
+    var course = $('#rcourse').find(":selected").val();
+    var branch = $('#rbranch').find(":selected").val();
+    var yos = +$('#ryos').find(":selected").val();
+    var data = {
+        "regno": $("#rregno").val(), 
+        "fn": $("#rfname").val(),
+        "ln": $("#rlname").val(),
+        "dob": $("#rdob").val(),
+        "email": $("#remail").val(),
+        "mob": $("#rmob").val(),
+        "gender": gender,
+        "clg": $("#rclg").val(),
+        "course": course,
+        "branch": branch,
+        "yos": yos,
+        "yoj": +$("#ryoj").val()};
+    console.log(data);
+    $.ajax({
+        url: "http://localhost:8080/students/register",
+        type: "POST",
+        contentType: "application/json",
+        data: JSON.stringify(data),
+        dataType: "json",
+        success: function (msg) {
+            console.log(msg);
+            $('#reg-text').html("Student details are registered successfully!");
+            $('#reg-status').toast({animation: false, delay: 2000});
+            $('#reg-status').toast('show');
+        },
+        error: function (req, status, error) {
+            $('#reg-text').html("Registration failed!");
+            $('#reg-status').toast({animation: false, delay: 2000});
+            $('#reg-status').toast('show');
+        }
     });
-});
+}
 
-app.get('/students/:regno', (req,res)=>{
-    var sql = "SELECT * FROM students WHERE RegistrationNo=?";
-    mysqlConnection.query(sql, [req.params.regno], (err, rows, fields)=> {
-     if (err) return res.send("No record found!");
-     res.send(rows);
+function onUpdate() {
+    var gender = +$('#ugender').find(":selected").val();
+    var course = $('#ucourse').find(":selected").val();
+    var branch = $('#ubranch').find(":selected").val();
+    var yos = +$('#uyos').find(":selected").val();
+    var data = {
+        "regno": $("#uregno").val(), 
+        "fn": $("#ufname").val(),
+        "ln": $("#ulname").val(),
+        "dob": $("#udob").val(),
+        "email": $("#uemail").val(),
+        "mob": $("#umob").val(),
+        "gender": gender,
+        "clg": $("#uclg").val(),
+        "course": course,
+        "branch": branch,
+        "yos": yos,
+        "yoj": +$("#uyoj").val()};
+    console.log(data);
+    $.ajax({
+        url: "http://localhost:8080/students/update",
+        type: "POST",
+        contentType: "application/json",
+        data: JSON.stringify(data),
+        dataType: "json",
+        success: function (msg) {
+            console.log(msg);
+            $('#update-text').html("Student details are updated successfully!");
+            $('#update-status').toast({animation: false, delay: 2000});
+            $('#update-status').toast('show');
+        },
+        error: function (req, status, error) {
+            $('#update-text').html("Update failed!");
+            $('#update-status').toast({animation: false, delay: 2000});
+            $('#update-status').toast('show');
+        }
     });
-});
+}
 
-app.delete('/students/:regno', (req,res)=>{
-    var sql = "DELETE FROM students WHERE RegistrationNo=?";
-    mysqlConnection.query(sql, [req.params.regno], (err, rows, fields)=> {
-     if (err) return res.send("Record not found!");
-     res.send("Student successfully deleted!");
+function onLoadStudentsView() {
+    var table_content =
+        '<table class="table" border="1" id="view_data">';
+    table_content += 
+        '<thead>\
+         <tr>\
+         <th scope="col">First Name</th>\
+         <th scope="col">Last Name</th>\
+         <th scope="col">Date Of Birth</th>\
+         <th scope="col">Email ID</th>\
+         <th scope="col">Mobile Number</th>\
+         <th scope="col">Gender</th>\
+         <th scope="col">College</th>\
+         <th scope="col">Course</th>\
+         <th scope="col">Branch</th>\
+         <th scope="col">Year Of Join</th>\
+         <th scope="col">Year Of Study</th>\
+         <th scope="col">Registration Number</th>\
+         <th scope="col">Update</th>\
+         <th scope="col">Delete</th>\
+         </tr>\
+         </thead>';
+    $.ajax({
+        url: "http://localhost:8080/students",
+        type: "GET",
+        contentType: "application/json",
+        data: "{}",
+        dataType: "json",
+        success: function (students) {
+            console.log(students);
+            table_content += '<tbody>';
+            for(var i in students) {
+                var student = students[i];
+                console.log(student);
+                var date = new Date(student.DateOfBirth);
+                var gender = (student.Gender==1)?"Male":((student.Gender==2)?"Female":"Other");
+                var course = (student.Course==1)?"B.Tech":"M.Tech";
+                table_content += 
+                    '<tr>\
+                    <td>'+student.FirstName+'</td>\
+                    <td>'+student.LastName+'</td>\
+                    <td>'+date.getFullYear()+"-"+("0"+(date.getMonth()+1)).slice(-2)+"-"+("0"+date.getDate()).slice(-2)+'</td>\
+                    <td>'+student.EmailID+'</td>\
+                    <td>'+student.MobileNo+'</td>\
+                    <td>'+gender+'</td>\
+                    <td>'+student.College+'</td>\
+                    <td>'+course+'</td>\
+                    <td>'+student.Branch+'</td>\
+                    <td>'+student.YearOfJoin+'</td>\
+                    <td>'+student.YearOfStudy+'</td>\
+                    <td>'+student.RegistrationNo+'</td>\
+                    <td><a href="update.html?regno='+student.RegistrationNo+'">Update</a></td>\
+                    <td><a href="" onclick="onDelete('+student.RegistrationNo+')">Delete</a></td>\
+                    </tr>';
+            }
+            table_content += '</tbody>';
+            $("#students-list").html(table_content);
+        },
+        error: function (req, status, error) {
+            console.log(req);
+            console.log(status);
+            console.log(error);
+            alert("Error try again");
+        }
     });
-});
+}
 
-app.put('/students', (req,res)=>{
-    let emp = req.body;
-    var sql = "INSERT INTO students (RegistrationNo, FirstName, LastName) VALUES (?, ?, ?)";
-    mysqlConnection.query(sql, [emp.regno, emp.fn, emp.ln], (err, rows, fields)=> {
-     if (err) return res.send("Insertion failed!");
-     res.send("Student successfully added!");
-    });
-});
+function getURLParameter(sParam)
+{
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) 
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) 
+        {
+            return sParameterName[1];
+        }
+    }
+}
 
-app.post('/students', (req,res)=>{
-    let emp = req.body;
-    var sql = "UPDATE students SET FirstName=?, LastName=? WHERE RegistrationNo=?";
-    mysqlConnection.query(sql, [emp.fn, emp.ln, emp.regno], (err, rows, fields)=> {
-     if (err) return res.send("Updation failed!");
-     res.send("Student successfully updated!");
+function onLoadUpdateView() {
+    var regno = getURLParameter("regno");
+    $("#uregno").val(regno);
+    $.ajax({
+        url: "http://localhost:8080/students/"+regno,
+        type: "GET",
+        contentType: "application/json",
+        data: "{}",
+        dataType: "json",
+        success: function (student) {
+            console.log(student);
+            var date = new Date(student.DateOfBirth);
+            console.log(date);
+            $("#ufname").val(student.FirstName);
+            $("#ulname").val(student.LastName);
+            $("#udob").val(date.getFullYear()+"-"+("0"+(date.getMonth()+1)).slice(-2)+"-"+("0"+date.getDate()).slice(-2));
+            $("#ugender").val(student.Gender);
+            $("#uemail").val(student.EmailID);
+            $("#uclg").val(student.College);
+            $("#umob").val(student.MobileNo);
+            $("#ucourse").val(student.Course);
+            $("#ubranch").val(student.Branch);
+            $("#uyoj").val(student.YearOfJoin);
+            $("#uyos").val(student.YearOfStudy);
+        },
+        error: function (req, status, error) {
+            console.log(req);
+            console.log(status);
+            console.log(error);
+            alert("Error try again");
+        }
     });
-});
+}
+
+function onDelete(regno) {
+    $.ajax({
+        url: "http://localhost:8080/students/"+regno,
+        type: "DELETE",
+        contentType: "application/json",
+        data: '{}',
+        dataType: "json",
+        success: function (msg) {
+            console.log(msg);
+            $('#del-text').html("Student is deleted successfully!");
+            $('#del-status').toast({animation: false, delay: 2000});
+            $('#del-status').toast('show');
+        },
+        error: function (req, status, error) {
+            $('#del-text').html("Deletion failed!");
+            $('#del-status').toast({animation: false, delay: 2000});
+            $('#del-status').toast('show');
+        }
+    });
+}
